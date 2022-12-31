0d9567e27e49988da56e6be445e00c22a5a2dd0a
Abesunbuggy <Abenezer@sunbuggyfunrentals.com>
2022-12-31 15:31:54 -0800

    made it all grid

--- index.php
+++ index.php
@@ -11,63 +11,110 @@
 
 <body>
     <main class="container">
-        <div class="first-item item-1">
+        <div class="introduction">
             <p>What is the difference between an ATV, UTV, and Dune Buggy?
             </p>
         </div>
-        <div class="item item-2 left">
-            <h1>ATVs</h1>
+        <div class="atv">
+            <div class="section left">
+                <h1>ATVs</h1>
+            </div>
+            <div class="section item-3 middle">
+                <div class="image-container">
+                    <div class="layer-0">
+
+                        <img class=" atv_side " src="assets/svgs/ATV Side Profile.svg" alt="Wheelbase: 48.5 inches Vehicle Width: 42 inches Seat Height: 32.5 inches Tire Size: 25 inches ">
+                    </div>
+                    <div class="layer-1">
+
+                        <img class="female_pic " src="assets/svgs/Silhouette Female.svg" alt="female">
+                    </div>
+                </div>
+                <div class="image-container">
+
+                    <div class="layer-0">
+                        <img class="atv_front" src="assets/svgs/ATV Front Profile.svg" alt="Wheelbase: 48.5 inches Vehicle Width: 42 inches Seat Height: 32.5 inches Tire Size: 25 inches ">
+                    </div>
+                    <div class="layer-1">
+                        <img class="male_pic " src="assets/svgs/Silhouette Male.svg" alt="male ">
+                    </div>
+
+                </div>
+
+
+
+
+            </div>
+            <div class="section item-4 text">
+                <p>All Terrain Vehicle aka Quad or Four Wheeler. Similar to a motorcycle with a straddle seating position and handlebars for steering. Acceleration, shifting, and braking are controlled by hand and foot levers. Short length, narrow width, and high seating position require operator to maintain balance on slopes or when turning, accelerating, or braking to avoid tipping or falling off. No safety restraints or protective structure means highest potential for injury. </p>
+            </div>
         </div>
-        <div class="item item-3 middle">
+        <div class="utv">
 
-            <img class="vehicles atv_pic" src="assets/svgs/ATV Front Profile.svg" alt="Wheelbase: 48.5 inches Vehicle Width: 42 inches Seat Height: 32.5 inches Tire Size: 25 inches ">
-            <img class="humans male_pic" src="assets/svgs/Silhouette Male.svg" alt="male ">
-            <img class="humans female_pic" src="assets/svgs/Silhouette Female.svg" alt="female ">
+            <div class="section item-5 left">
+                <h1>UTVs</h1>
+            </div>
+            <div class="section item-6 middle">
+                <div class="image-container">
+                    <div class="layer-1">
 
-            <img class="vehicles atv_pic" src="assets/svgs/ATV Side Profile.svg" alt="Wheelbase: 48.5 inches Vehicle Width: 42 inches Seat Height: 32.5 inches Tire Size: 25 inches ">
+                        <img class="humans male_pic" src="assets/svgs/Silhouette Male.svg" alt="male ">
+                    </div>
+                    <div class="layer-0">
 
+                        <img class="utv_front" src="assets/svgs/RZR Front Profile.svg" alt="Wheelbase: 79 inches Vehicle Width: 60 inches Seat Height: 28 inches Tire Size: 25 inches ">
+                    </div>
+
+                </div>
+                <div class="image-container">
+                    <div class="layer-1">
+
+                        <img class="humans female_pic" src="assets/svgs/Silhouette Female.svg" alt="female ">
+                    </div>
+                    <div class="layer-0">
+
+                        <img class="vehicles utv_side" src="assets/svgs/RZR Side Profile.svg" alt="Wheelbase: 79 inches Vehicle Width: 60 inches Seat Height: 28 inches Tire Size: 25 inches ">
+                    </div>
+
+                </div>
+            </div>
+            <div class="section item-7 text">
+                <p>Utility Terrain Vehicle aka Side by Side. Similar to a golf cart with occupants seated up high above engine and a steering wheel for directional control. Acceleration and braking are controlled by foot pedals. Short length, narrow width, and high seating position require operator to excersise caution when driving on uneven, sloped, or soft surfaces to prevent tipping vehicle or flipping forward when braking. 3 or 4 point seat belts with a Roll Cage. Some UTVs have half doors or a screen to prevents arms from coming off during a roll-over or flip. </p>
+            </div>
         </div>
-        <div class="item item-4 text">
+        <div class="dunebuggy">
+            <div class="section item-8 left">
+                <h1>Dunebuggies</h1>
+            </div>
+            <div class="section item-9 middle">
+                <div class="image-container">
+                    <div class="layer-0">
 
-            <p>All Terrain Vehicle aka Quad or Four Wheeler. Similar to a motorcycle with a straddle seating position and handlebars for steering. Acceleration, shifting, and braking are controlled by hand and foot levers. Short length, narrow width, and high seating position require operator to maintain balance on slopes or when turning, accelerating, or braking to avoid tipping or falling off. No safety restraints or protective structure means highest potential for injury. </p>
+                        <img class="vehicles buggy_front" src="assets/svgs/Buggy Front Profile.svg" alt="Wheelbase: 102 inches Vehicle Width: 78 inches Seat Height: 17 inches Tire Size: 33 inches rear, 28.5 inches front ">
+                    </div>
+                    <div class="layer-1">
+                        <img class="humans male_pic" src="assets/svgs/Silhouette Male.svg" alt="male ">
 
-        </div>
-        <div class="item item-5 left">
-            <h1>UTVs</h1>
-        </div>
-        <div class="item item-6 middle">
+                    </div>
+                </div>
+                <div class="image-container">
+                    <div class="layer-0">
+                        <img class="vehicles buggy_side" src="assets/svgs/Buggy Side Profile.svg" alt="Wheelbase: 102 inches Vehicle Width: 78 inches Seat Height: 17 inches Tire Size: 33 inches rear, 28.5 inches front ">
 
+                    </div>
+                    <div class="layer-1">
 
-            <img class="vehicles utv_pic" src="assets/svgs/RZR Front Profile.svg" alt="Wheelbase: 79 inches Vehicle Width: 60 inches Seat Height: 28 inches Tire Size: 25 inches ">
-            <img class="humans male_pic" src="assets/svgs/Silhouette Male.svg" alt="male ">
-            <img class="humans female_pic" src="assets/svgs/Silhouette Female.svg" alt="female ">
-            <img class="vehicles utv_pic" src="assets/svgs/RZR Side Profile.svg" alt="Wheelbase: 79 inches Vehicle Width: 60 inches Seat Height: 28 inches Tire Size: 25 inches ">
-
-        </div>
-        <div class="item item-7 text">
-
-            <p>Utility Terrain Vehicle aka Side by Side. Similar to a golf cart with occupants seated up high above engine and a steering wheel for directional control. Acceleration and braking are controlled by foot pedals. Short length, narrow width, and high seating position require operator to excersise caution when driving on uneven, sloped, or soft surfaces to prevent tipping vehicle or flipping forward when braking. 3 or 4 point seat belts with a Roll Cage. Some UTVs have half doors or a screen to prevents arms from coming off during a roll-over or flip. </p>
-
-        </div>
-        <div class="item item-8 left">
-            <h1>Dunebuggies</h1>
-        </div>
-        <div class="item item-9 middle">
-
-
-            <img class="vehicles buggy_pic" src="assets/svgs/Buggy Front Profile.svg" alt="Wheelbase: 102 inches Vehicle Width: 78 inches Seat Height: 17 inches Tire Size: 33 inches rear, 28.5 inches front ">
-            <img class="humans male_pic" src="assets/svgs/Silhouette Male.svg" alt="male ">
-            <img class="humans female_pic" src="assets/svgs/Silhouette Female.svg" alt="female ">
-            <img class="vehicles buggy_pic" src="assets/svgs/Buggy Side Profile.svg" alt="Wheelbase: 102 inches Vehicle Width: 78 inches Seat Height: 17 inches Tire Size: 33 inches rear, 28.5 inches front ">
-
-        </div>
-        <div class="item item-10 text">
-
-            <p>Dune Buggy is a specialty vehicle adapted for steep slopes and soft surfaces encountered when traversing off road terrain. Occupants sit low between the wheels in a car like bucket seat with a steering wheel for directional control. Acceleration and braking are controlled by foot pedals. Long length, wide width, and low seating position provides for the most stable type of Off Road vehicle. Designed to traverse steep slopes and soft terrain with very low risk of tipping. Like a sports car for Off Roading. 4 point seat belts and double loop steel Roll Cage to protect occupants.
-
+                        <img class="humans female_pic" src="assets/svgs/Silhouette Female.svg" alt="female ">
+                    </div>
+                </div>
+            </div>
+            <div class="section item-10 text">
+                <p>Dune Buggy is a specialty vehicle adapted for steep slopes and soft surfaces encountered when traversing off road terrain. Occupants sit low between the wheels in a car like bucket seat with a steering wheel for directional control. Acceleration and braking are controlled by foot pedals. Long length, wide width, and low seating position provides for the most stable type of Off Road vehicle. Designed to traverse steep slopes and soft terrain with very low risk of tipping. Like a sports car for Off Roading. 4 point seat belts and double loop steel Roll Cage to protect occupants.
+            </div>
         </div>
         </div>
     </main>
+
 </body>
 
 </html>
